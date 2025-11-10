9af2baeaee2eb7b069e94fc8928a4fc209b54513
Lorenz Bernard Marqueses <lorenz_marqueses@dlsu.edu.ph>
2025-11-11 01:12:41 +0800

    Move parse() to riscv.js

--- src/lib/riscv/riscv.js
+++ src/lib/riscv/riscv.js
@@ -8,6 +8,22 @@ import { slliExecute, slliPack } from '$lib/riscv/instructions/slli.js';
 import { sltExecute, sltPack } from '$lib/riscv/instructions/slt.js';
 import { swExecute, swPack } from '$lib/riscv/instructions/sw.js';
 
+const sections = {
+    NONE: 0,
+    DATA: 1,
+    TEXT: 2
+}
+const regNames = [
+    // Named registers (aliases)
+    ...registersInt.keys(),
+    // x0, x1, ... x31
+    ...Array.from({ length: 32 }, (_, i) => `x${i}`)
+]
+const hexAllowedRegex = /^0x[0-9a-fA-F]+$/;
+const binAllowedRegex = /^0b[0-1]+$/;
+const decAllowedRegex = /^-?[0-9]+$/;
+const locNameAllowedRegex = /[0-9a-zA-Z]:$/;
+
 /** Splits a line of code into its first token and the rest of the line */
 const getParts = (line) => {
     let parts = line.trim().split(/\s+/)
@@ -15,67 +31,236 @@ const getParts = (line) => {
     let head = parts[0].trim()
     let tail = parts.slice(1).join(' ').trim()
 
-
     if (!head && !tail) {
-        // If both head and tail are empty, return null
-        return null
+        // If both head and tail are empty, return whitespace
+        return " "
     } else {
         // Otherwise, return the non-empty parts
         return [head, tail].filter(Boolean)
     }
 }
 
-const parseInstruction = (line) => {
-
-}
-
-const parseDirective = (line) => {
-    // I think we only care about .word right now
-}
-
-const parseLabel = (line) => {
-
-}
-
 /** Parses RISC-V code. */
 export const parse = (input) => {
+    // Return value. Object representation of the program
+    const programData = {
+        globlNames: [],
+        branchNames: [],
+        branchLineNos: [],
+        locNames: [],
+        // TODO: Put packed instructions into here
+        instructions: [],
+    }
+
     // Split into lines
     // Do not ignore empty lines since that's needed for error line count
     let lines = input.split(/\r?\n/)
 
     // Handle comments
     lines = lines.map(line => {
-            line = line.split('#')[0]
-            line.trim()
-            return line
+        line = line.split('#')[0]
+        line.trim()
+        return line
     })
 
     // Tokenize per line and remove comments
-    lines = lines.map(line => getParts(line)
-    ).map(line => {
-        if(line === null) {
-            return " "
+    lines = lines.map(line => getParts(line))
+
+    let currentSection = sections.NONE
+
+    mainLoop:
+    for(let [index, line] of lines.entries()) {
+        let i = index + 1 // True line number is +1
+
+        // If instruction comes after location name
+        if(line.length > 1 && currentSection == sections.TEXT && locNameAllowedRegex.test(line[0])) {
+            programData.locNames.push(line[0].slice(0, -1))
+            let dump = line.shift()
+            line = line[0].split(/\s+(.*)/);
         }
-        return line
-    }) // Avoid dealing with NULL
 
-    // Go over each line
-    for (let line in lines) {
-        // line[0] is the "head" of the line and can be a instruction, a
-        // directive, or a label. line[1] is the rest of the line. Both are
-        // guaranteed to have been trimmed already.
+        if(line[0] === " ") {
+            // Do nothing
+        }
 
-        if (line[0].startsWith('.')) {
-            // Treat as directive
-            parseDirective(line)
-        } else if (line[0].endsWith(':')) {
-            // Treat as label
-            parseLabel(line)
-        } else {
-            // Treat as instruction.
-            parseInstruction(line)
+        // globl, data, text directives ==========================================================
+
+        // .globl
+        else if(line[0].toLowerCase() === ".globl") {
+            // Cannot declare more than one global function in the same line
+            if(line.length == 2 && !/\s/.test(line[1])) {
+                programData.globlNames.push(line[1])
+                currentSection = sections.NONE
+            } else {
+                throw { message: "Invalid .globl declaration", line: i }
+            }
+        }
+        // .data
+        else if(line[0].toLowerCase() === ".data") {
+            if(line.length == 1 && !/\s/.test(line[0])) {
+                currentSection = sections.DATA
+            } else {
+                throw { messasge: "Invalid .data declaration", line: i }
+            }
+        }
+        // .text
+        else if(line[0].toLowerCase() === ".text") {
+            if(line.length == 1 && !/\s/.test(line[0])) {
+                currentSection = sections.TEXT
+            } else {
+                throw { message: "Invalid .text declaration", line: i }
+            }
+        }
+        // Labeled instructions (branchable locations)
+        else if(currentSection == sections.TEXT && locNameAllowedRegex.test(line[0])) {
+            console.log("HERE")
+            programData.locNames.push(line[0].slice(0, -1))
+        }
+        // We're not dealing with single-word instructions
+        else if(line.length == 1) {
+            throw { message: line[0].split(" ")[0] + " is not a recognized instruction", line: i}
+        }
+
+        // Data declarations =====================================================================
+
+        else if(currentSection == sections.DATA) {
+            if(!locNameAllowedRegex.test(line[0])) {
+                throw { message: "Invalid data declaration", line: i }
+            }
+            // Needs to be this to account for commas
+            else if(!line[1].toLowerCase().startsWith(".word ")) {
+                throw { message: "μRISC-V only supports .word", line: i }
+            } else {
+                // CHECK EACH VALUE INDIVIDUALLY
+                let dataArgs = line[1].toLowerCase().replace(/,/g, "").split(" ");
+                let dump = dataArgs.shift()
+
+                for(let j in dataArgs) {
+                    if(hexAllowedRegex.test(dataArgs[j]) || binAllowedRegex.test(dataArgs[j])
+                        || decAllowedRegex.test(dataArgs[j])) {
+                        // TODO: Insert into memory
+                    } else {
+                        throw { message: "Invalid variable declaration", line: i }
+                    }
+                }
+            }
+        }
+
+        // Inside .text ===========================================================================
+
+        else if(currentSection == sections.TEXT) { // you can declare stuff outside of a function apparently
+            let args = line[1].toLowerCase().replace(/,/g, "").split(" ")
+
+            // nemurihiMEEEEEEE mezameru watashi wa iMAAAAAAAAAAAA
+            // lw instruction
+            if(line[0].toLowerCase() === "lw") {
+                let numArg = args[1].split("(")[0]
+                let innerArg = args[1].split("(")[1].slice(0, -1)
+
+                if(!regNames.includes(args[0])) {
+                    throw { message: "Invalid operands", line: i }
+                } else if(args.length != 2) {
+                    throw { message: "Invalid number of operands", line: i }
+                } else if(regNames.includes(innerArg) &&
+                    (hexAllowedRegex.test(numArg) || binAllowedRegex.test(numArg)
+                    || decAllowedRegex.test(numArg))) {
+                    // TODO: LW call
+                } else {
+                    throw { message: "Invalid operands", line: i }
+                }
+            }
+            // sw instruction
+            else if(line[0].toLowerCase() === "sw") {
+                let numArg = args[1].split("(")[0]
+                let innerArg = args[1].split("(")[1].slice(0, -1)
+
+                if(!regNames.includes(args[0])) {
+                    throw { message: "Invalid operands", line: i }
+                } else if(args.length != 2) {
+                    throw { message: "Invalid number of operands", line: i }
+                } else if(regNames.includes(innerArg) &&
+                    (hexAllowedRegex.test(numArg) || binAllowedRegex.test(numArg)
+                    || decAllowedRegex.test(numArg))) {
+                    // TODO: SW call
+                } else {
+                    throw { message: "Invalid operands", line: i }
+                }
+            }
+            // slt instruction
+            else if(line[0].toLowerCase() === "slt") {
+                if(args.length != 3) {
+                    throw { message: "Invalid number of operands", line: i }
+                } else if(regNames.includes(args[0]) && regNames.includes(args[1])
+                    && regNames.includes(args[2])) {
+                    // TODO: SLT call
+                } else {
+                    throw { message: "Invalid operands", line: i }
+                }
+            }
+            // sll instruction
+            else if(line[0].toLowerCase() === "sll") {
+                if(args.length != 3) {
+                    throw { message: "Invalid number of operands", line: i }
+                } else if(regNames.includes(args[0]) && regNames.includes(args[1])
+                    && regNames.includes(args[2])) {
+                    // TODO: SLL call
+                } else {
+                    throw { message: "Invalid operands", line: i }
+                }
+            }
+            // slli instruction
+            else if(line[0].toLowerCase() === "slli") {
+                if(args.length != 3) {
+                    throw { message: "Invalid number of operands", line: i }
+                } else if(regNames.includes(args[0]) && regNames.includes(args[1]) &&
+                    (hexAllowedRegex.test(args[2]) || binAllowedRegex.test(args[2])
+                    || decAllowedRegex.test(args[2]))) {
+                    // TODO: SLLI call
+                } else {
+                    throw { message: "Invalid operands", line: i }
+                }
+            }
+            // beq instruction
+            else if(line[0].toLowerCase() === "beq") {
+                if(args.length != 3) {
+                    throw { message: "Invalid number of operands", line: i }
+                } else if(!regNames.includes(args[0]) || !regNames.includes(args[1])) {
+                    console.log(args[1])
+                    throw { message: "Invalid operands", line: i }
+                } else {
+                    // To consider capitalization
+                    programData.branchNames.push(line[1].replace(/,/g, "").split(" ")[2])
+                    programData.branchLineNos.push(i)
+                }
+            }
+            // blt instruction
+            else if(line[0].toLowerCase() === "blt") {
+                if(args.length != 3) {
+                    throw { message: "Invalid number of operands", line: i }
+                } else if(!regNames.includes(args[0]) || !regNames.includes(args[1])) {
+                    throw { message: "Invalid operands", line: i }
+                } else {
+                    programData.branchNames.push(line[1].replace(/,/g, "").split(" ")[2])
+                    programData.branchLineNos.push(i)
+                }
+            }
+            // Fail to match with any valid instructions
+            else {
+                throw { message: line[0].split(" ")[0] + " is not a recognized instruction", line: i }
+            }
         }
     }
 
-    return lines
+    // Check if branch names are valid
+
+    for(let i in programData.branchNames) {
+        if(!programData.locNames.includes(programData.branchNames[i])) {
+            throw {
+                message: "Branch location " + programData.branchNames[i] + " does not exist",
+                line: programData.branchLineNos[i]
+            }
+        }
+    }
+
+    return programData
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,279 +8,22 @@
     let editor;
     let startingAddress = 0x0000;
 
-    // Error detection
-    let errorDetected = false;
-    let errorLineNumber;
-    let errorDescription;
-
-    // Error function
-    function errorDisplay(lineNum, desc) {
-        errorDetected = true
-        errorLineNumber = parseInt(lineNum) + 1
-        errorDescription = desc
-        editor.highlightLine(errorLineNumber)
-        abruptEnd = true;
-    }
+    // Reactive  for error handling
+    let error = null;
 
     const run = () => {
-        // Test function: Stores 4-byte integer -99 at address 0x0000
-        memory.storeInteger(0x0000, -99, 4)
+        try {
+            // Reset error objcet
+            error = null
 
-        errorDetected = false
-        let parsedLines = parse(editor.getValue())
+            // Parse program
+            let program = parse(editor.getValue())
 
-        let globlNames = new Array()    // names of global functions
-        let branchNames = new Array()
-        let branchLineNos = new Array()
-        let locNames = new Array()
-        let isData = false
-        let isText = false
-        let isFunc = false
-        let abruptEnd = false
-
-        let regNames = ["zero", "ra", "sp", "gp", "tp", "t0", "t1", "t2", "s0", "s1",
-                        "a0", "a1", "a2", "a3", "a4", "a5", "a6", "a7", "s2", "s3",
-                        "s4", "s5", "s6", "s7", "s8", "s9", "s10", "s11", "t3", "t4",
-                        "t5", "t6", "pc",
-                        "x0", "x1", "x2", "x3", "x4", "x5", "x6", "x7", "x8", "x9", "x10",
-                        "x11", "x12", "x13", "x14", "x15", "x16", "x17", "x18", "x19", "x20",
-                        "x21", "x22", "x23", "x24", "x25", "x26", "x27", "x28", "x29", "x30",
-                        "x31"]
-
-        const hexAllowedRegex = /^0x[0-9a-fA-F]+$/;
-        const binAllowedRegex = /^0b[0-1]+$/;
-        const decAllowedRegex = /^-?[0-9]+$/;
-        const locNameAllowedRegex = /[0-9a-zA-Z]:$/;
-
-        mainLoop:
-        for(let i in parsedLines) {
-            let line = parsedLines[i]
-
-            // If instruction comes after location name
-            if(line.length > 1 && isText && locNameAllowedRegex.test(line[0])) {
-                isData = false
-                isFunc = true
-                locNames.push(line[0].slice(0, -1))
-
-                let dump = line.shift()
-                line = line[0].split(/\s+(.*)/);
-            }
-
-            console.log(line)
-
-            if(line[0] === " ") { // Instead of null, Editor turns it into " "
-                // Do nothing
-            }
-
-            // globl, data, text directives ==========================================================
-            else if(line[0].toLowerCase() === ".globl") {
-                // cannot declare more than one global function in the same line
-                if(line.length == 2 && !/\s/.test(line[1])) {
-                    globlNames.push(line[1])
-                    isData = false
-                    isText = false
-                    isFunc = false
-                }
-                else {
-                    errorDisplay(i, "Invalid .globl declaration")
-                    break
-                }
-            }
-            else if(line[0].toLowerCase() === ".data") {
-                if(line.length == 1 && !/\s/.test(line[0])) {
-                    isData = true
-                    isText = false
-                    isFunc = false
-                }
-                else {
-                    errorDisplay(i, "Invalid .data declaration")
-                    break
-                }
-            }
-            else if(line[0].toLowerCase() === ".text") {
-                if(line.length == 1 && !/\s/.test(line[0])) {
-                    isData = false
-                    isText = true
-                    isFunc = false
-                }
-                else {
-                    errorDisplay(i, "Invalid .text declaration")
-                    break
-                }
-            }
-            else if(isText && locNameAllowedRegex.test(line[0])) {
-                isData = false
-                isFunc = true
-                locNames.push(line[0].slice(0, -1))
-            }
-            else if(line.length == 1) {
-                errorDisplay(i, line[0].split(" ")[0] + " is not a recognized operator")
-                break
-            }
-            // Data declaration =====================================================================
-            else if(isData) {
-                if(!locNameAllowedRegex.test(line[0])) {
-                    errorDisplay(i, "Invalid data declaration")
-                    break
-                }
-                else if(!line[1].toLowerCase().startsWith(".word ")) { // needs to be this to account for commas
-                    errorDisplay(i, "μRISC-V only supports .word")
-                    break
-                }
-                else {
-                    // CHECK EACH VALUE INDIVIDUALLY
-                    let dataArgs = line[1].toLowerCase().replace(/,/g, "").split(" ");
-                    let dump = dataArgs.shift()
-
-                    for(let j in dataArgs) {
-                        if(hexAllowedRegex.test(dataArgs[j]) || binAllowedRegex.test(dataArgs[j])
-                           || decAllowedRegex.test(dataArgs[j])) {
-                            // TODO: Insert into memory
-                        }
-                        else {
-                            errorDisplay(i, "Invalid variable declaration")
-                            break mainLoop;
-                        }
-                    }
-                }
-            }
-            // Inside .text ===========================================================================
-            else if(isText) { // you can declare stuff outside of a function apparently
-                let args = line[1].toLowerCase().replace(/,/g, "").split(" ")
-
-                // nemurihiMEEEEEEE mezameru watashi wa iMAAAAAAAAAAAA
-                if(line[0].toLowerCase() === "lw") {
-                    let numArg = args[1].split("(")[0]
-                    let innerArg = args[1].split("(")[1].slice(0, -1)
-
-                    if(!regNames.includes(args[0])) {
-                        errorDisplay(i, "Invalid operands")
-                        break
-                    }
-                    else if(args.length != 2) {
-                        errorDisplay(i, "Invalid number of operands")
-                        break
-                    }
-                    else if(regNames.includes(innerArg) &&
-                            (hexAllowedRegex.test(numArg) || binAllowedRegex.test(numArg)
-                             || decAllowedRegex.test(numArg))) {
-                        // TODO: LW call
-                    }
-                    else {
-                        errorDisplay(i, "Invalid operands")
-                        break
-                    }
-                }
-                else if(line[0].toLowerCase() === "sw") {
-                    let numArg = args[1].split("(")[0]
-                    let innerArg = args[1].split("(")[1].slice(0, -1)
-
-                    if(!regNames.includes(args[0])) {
-                        errorDisplay(i, "Invalid operands")
-                        break
-                    }
-                    else if(args.length != 2) {
-                        errorDisplay(i, "Invalid number of operands")
-                        break
-                    }
-                    else if(regNames.includes(innerArg) &&
-                            (hexAllowedRegex.test(numArg) || binAllowedRegex.test(numArg)
-                             || decAllowedRegex.test(numArg))) {
-                        // TODO: SW call
-                    }
-                    else {
-                        errorDisplay(i, "Invalid operands")
-                        break
-                    }
-                }
-                else if(line[0].toLowerCase() === "slt") {
-                    if(args.length != 3) {
-                        errorDisplay(i, "Invalid number of operands")
-                        break
-                    }
-                    else if(regNames.includes(args[0]) && regNames.includes(args[1])
-                            && regNames.includes(args[2])) {
-                        // TODO: SLT call
-                    }
-                    else {
-                        errorDisplay(i, "Invalid operands")
-                        break
-                    }
-                }
-                else if(line[0].toLowerCase() === "sll") {
-                    if(args.length != 3) {
-                        errorDisplay(i, "Invalid number of operands")
-                        break
-                    }
-                    else if(regNames.includes(args[0]) && regNames.includes(args[1])
-                            && regNames.includes(args[2])) {
-                        // TODO: SLL call
-                    }
-                    else {
-                        errorDisplay(i, "Invalid operands")
-                        break
-                    }
-                }
-                else if(line[0].toLowerCase() === "slli") {
-                    if(args.length != 3) {
-                        errorDisplay(i, "Invalid number of operands")
-                        break
-                    }
-                    else if(regNames.includes(args[0]) && regNames.includes(args[1]) &&
-                            (hexAllowedRegex.test(args[2]) || binAllowedRegex.test(args[2])
-                             || decAllowedRegex.test(args[2]))) {
-                        // TODO: SLLI call
-                    }
-                    else {
-                        errorDisplay(i, "Invalid operands")
-                        break
-                    }
-                }
-                else if(line[0].toLowerCase() === "beq") {
-                    if(args.length != 3) {
-                        errorDisplay(i, "Invalid number of operands")
-                        break
-                    }
-                    else if(!regNames.includes(args[0]) || !regNames.includes(args[1])) {
-                        errorDisplay(i, "Invalid operands")
-                        break
-                    }
-                    else {
-                        // To consider capitalization
-                        branchNames.push(line[1].replace(/,/g, "").split(" ")[2])
-                        branchLineNos.push(i)
-                    }
-                }
-                else if(line[0].toLowerCase() === "blt") {
-                    if(args.length != 3) {
-                        errorDisplay(i, "Invalid number of operands")
-                        break
-                    }
-                    else if(!regNames.includes(args[0]) || !regNames.includes(args[1])) {
-                        errorDisplay(i, "Invalid operands")
-                        break
-                    }
-                    else {
-                        branchNames.push(line[1].replace(/,/g, "").split(" ")[2])
-                        branchLineNos.push(i)
-                    }
-                }
-                else {
-                    errorDisplay(i, line[0].split(" ")[0] + " is not a recognized operator")
-                    break
-                }
-            }
+            // Do stuff with program
+            console.log(program)
+        } catch (e) {
+            error = e;
         }
-
-        // Check if branch names are valid
-
-        if(!abruptEnd)
-            for(let i in branchNames) {
-                if(!locNames.includes(branchNames[i])) {
-                    errorDisplay(branchLineNos[i], "Branch location " + branchNames[i] + " does not exist")
-                    break
-                }
-            }
     }
 
     const loadASM = async (e) => {
@@ -296,8 +39,8 @@
 <button on:click={run}>Run</button>
 <input type="file" accept=".asm" on:change={loadASM} />
 
-{#if errorDetected}
-  <h3 style="color: red;">Error in line {errorLineNumber}. {errorDescription}</h3>
+{#if error}
+  <h3 style="color: red;">Error in line {error.line}. {error.message}</h3>
 {/if}
 
 <h2>Memory viewer</h2>
